d4681f681a199d92c965a33bc5f11398d221780f
KomelAbbas <komelabbasweb786@gmail.com>
2023-11-21 19:39:38 +0530

    add invoice table

---
+++ dist/components/InvoiceTable.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const InvoiceTable = () => {
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "App"
+  }, "Invoice Table", /*#__PURE__*/_react.default.createElement("br", null));
+};
+var _default = exports.default = InvoiceTable;
--- dist/index.js
+++ dist/index.js
@@ -9,5 +9,12 @@ Object.defineProperty(exports, "InvoiceForm", {
     return _InvoiceForm.default;
   }
 });
+Object.defineProperty(exports, "InvoiceTable", {
+  enumerable: true,
+  get: function get() {
+    return _InvoiceTable.default;
+  }
+});
 var _InvoiceForm = _interopRequireDefault(require("./components/InvoiceForm"));
+var _InvoiceTable = _interopRequireDefault(require("./components/InvoiceTable"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
